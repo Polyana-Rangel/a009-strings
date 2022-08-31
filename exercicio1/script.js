0b859d53ea3cfd407bf476aeb9e1f5ad287f9334
Polyana-Rangel <polyanarangel17@gmail.com>
2022-08-31 13:41:55 -0300

    exercicio1

---
+++ exercicio1/script.js
@@ -0,0 +1,6 @@
+const nome = prompt("Digite seu nome:");
+let Comida1 = prompt ("Digite sua primeira comida favorita");
+let Comida2 = prompt ("Digite sua segunda comida favorita");
+let Comida3 = prompt ("Digite sua terceira comida favorita");
+
+console.log (`Estas são as comidas favoritas de ${nome} \n ${Comida1}\n ${Comida2}\n ${Comida3}`)
